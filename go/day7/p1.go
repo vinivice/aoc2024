c439cd1085b3fade1ba0555ba038eccd0a2a7b06
Vinícius Andrade <vinivice@gmail.com>
2024-12-07 18:27:12 -0300

    Day7 done in go

---
+++ go/day7/p1.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func checkOperation(target, lh int, inputs []string, operator string) bool {
+    if len(inputs) == 0 {
+        return target == lh 
+    }
+
+    rh, _ := strconv.Atoi(inputs[0])
+    newLh := 0
+    switch operator {
+    case "+":
+        newLh = lh + rh
+    case "*":
+        newLh = lh * rh
+    }
+
+    newInputs := inputs[1:]
+
+    if checkOperation(target, newLh, newInputs, "+") {
+        return true
+    }
+    
+    return checkOperation(target, newLh, newInputs, "*")
+}
+
+func checkMatch(target int, inputs []string) int {
+    if checkOperation(target, 0, inputs, "+") || checkOperation(target, 0, inputs, "*") {
+        return target
+    }
+
+    return 0
+    
+}
+
+func main() {
+    //file, _ := os.Open("day7_input_small")
+    file, _ := os.Open("day7_input")
+
+    scanner := bufio.NewScanner(file)
+
+
+    total := 0
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        values := strings.Split(line, ":")
+
+        target, _ := strconv.Atoi(values[0])
+        inputs := strings.Split(values[1], " ")
+
+        total += checkMatch(target, inputs)
+    }
+
+    fmt.Println(total)
+}
